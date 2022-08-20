88b3b56e056c7a68452fcd70db3ba369a80bb639
johnk2022 <keenan.john@hotmail.com>
2022-08-20 15:45:58 +0000

    Initial commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -3,10 +3,32 @@
  * command node the-spread-operator.js in the terminal
  */
 
-// No spread operator
+// No spread operator.  aa2 just points to arr1 so any changes are made to both JK
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("Second array", arr2);
+console.log("First array", arr1);
 
-// Copying an array
+
+
+// Copying an array  ...arr3 makes an actual copy of arr3 JK
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log("Third array", arr3);
+console.log("Forth array", arr4);
 
 // Copying an object
+let obj1 = {a: 1, b: 2, c: 3 };
+let obj2 = {...obj1, d: 4 };
+let obj3 = {...obj1, b: 5 };
+
+console.log("Object 1", obj1);
+console.log("Object 2", obj2);
+console.log("Object 3", obj3);
 
 // Copying only part of an array/object
+let arr5 = [...arr1, {...obj1 }, ...arr3, "x", "y", "z"];
+console.log("Fifth array", arr5);
